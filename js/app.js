2a2d43a5399823aa88467d9b9d43c22edf42aeac
justzxid <zaidmk96@gmail.com>
2025-01-19 14:18:09 +0300

    HTMLandCSSstructure

---
+++ js/app.js
@@ -0,0 +1,41 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const symbols = ['🦅','🦍','🦔','🦜','🦑','🦚','🐢','🐎','🐈','🐕','🦘','🐄','🐓','🦆','🐳','🐬']
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let firstCard;
+let secondCard;
+let chances = 10;
+
+/*------------------------ Cached Element References ------------------------*/
+
+const chanceLeft = document.querySelector('#chance')
+const totalScore = document.querySelector('#score')
+const gameBoard = document.querySelector('#board')
+const cards = document.querySelectorAll('.sqr')
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*----------------------------- Psuedo Code -----------------------------*/
+
+/* 
+
+The cards should be initalized randomly and should be different after reseting.
+The cards should contain event listeners that register a click event.
+The cards should be able to display the icon it represents underneath.
+The first selected card must be differentiated against the second selected card.
+If the cards do not match, then both should return back to their inital stage and reduce chances by 1.
+
+
+
+
+
+
+*/
