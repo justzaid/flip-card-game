86a7ce23df81e301a171a6a6239ac16034a405ca
justzxid <zaidmk96@gmail.com>
2025-01-20 17:10:13 +0300

    updatedCommit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,63 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const symbols = ['🦅','🦍','🦔','🦜','🦑','🦚','🐢','🐎','🐈','🐕','🦘','🐄','🐓','🦆','🐳','🐬']
+const symbols = [
+    {   icon: '🦅',
+        name: 'eagle'
+    },
+    {   icon: '🦅',
+        name: 'eagle'
+    },
+    {   icon: '🦜',
+        name: 'parrot'
+    },     
+    {   icon: '🦜',
+        name: 'parrot'
+    },
+    {   icon: '🦑',
+        name: 'octopus'
+    },
+    {   icon: '🦑',
+        name: 'octopus'
+    },
+    {   icon: '🐢',
+        name: 'turtle'
+    },
+    {   icon: '🐢',
+        name: 'turtle'
+    },
+    {   icon: '🐎',
+        name: 'horse'
+    },
+    {   icon: '🐎',
+        name: 'horse'
+    },
+    {   icon: '🦍',
+        name: 'gorilla'
+    },
+    {   icon: '🦍',
+        name: 'gorilla'
+    },
+    {   icon: '🦆',
+        name: 'duck'
+    },
+    {   icon: '🦆',
+        name: 'duck'
+    },
+    {   icon: '🐳',
+        name: 'whale'
+    }, 
+    {   icon: '🐳',
+        name: 'whale'
+    }
+
+]
+
 
 /*---------------------------- Variables (state) ----------------------------*/
 
 let firstCard;
 let secondCard;
+let checkBoard = false;
 let chances = 10;
 
 /*------------------------ Cached Element References ------------------------*/
@@ -17,16 +69,57 @@ const cards = document.querySelectorAll('.sqr')
 
 /*-------------------------------- Functions --------------------------------*/
 
-function cardToggle(event) {
-    console.log('I am clicked')
+// Game state initalizer
+
+function render() {
+    console.log('Game is loading')
 }
 
 
+// Card Toggler - When clicked
+
+function cardToggle(event) {
+    const cardId = event.target.id;
+    event.target.textContent = symbols[cardId].icon;
+    console.log(symbols[cardId].name)
+    
+}
+
+
+// Card shuffling / Card Randomizer Function
+
+function shuffleCards(symbols) {
+    let currentIdx = symbols.length;
+    while (currentIdx != 0) {
+        let randomIdx = Math.floor(Math.random() * currentIdx);
+        currentIdx--;
+        [symbols[currentIdx], symbols[randomIdx]] = [
+            symbols[randomIdx], symbols[currentIdx]];
+    }
+}
+
+// Check if the cards match
+
+
+function checkIfMatch(firstCard) {
+    if (firstCard === secondCard) {
+        return;
+    } 
+}
+
 /*----------------------------- Event Listeners -----------------------------*/
 
+// Event listener that registers a click and runs the cardToggle function
+
 cards.forEach(card => {
-    card.addEventListener('click', cardToggle)
+    card.addEventListener('click', cardToggle);
 });
+
+
+/*--------------------------------- Function Calling--------------------------------------*/
+
+render()
+shuffleCards(symbols)
 
 /*----------------------------- Psuedo Code -----------------------------*/
 
@@ -37,9 +130,6 @@ The cards should contain event listeners that register a click event.
 The cards should be able to display the icon it represents underneath.
 The first selected card must be differentiated against the second selected card.
 If the cards do not match, then both should return back to their inital stage and reduce chances by 1.
-
-
-
 
 
 
